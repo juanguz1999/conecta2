6882e6abbfde4ba098c104456c0daf4b18c7e5bd
juanguz1999 <55995911+juanguz1999@users.noreply.github.com>
2023-05-19 17:45:27 -0500

    Hola Mundo

---
+++ src/main/java/com/colegios_peruanos/conectados/ConectadosApplication.java
@@ -0,0 +1,21 @@
+package com.colegios_peruanos.conectados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+public class ConectadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectadosApplication.class, args);
+	}
+
+	@GetMapping
+	String holaMundo(){
+		return "hola-mundo";
+	}
+
+}
