6d8a03b6366108e6adac6e98001f0657d84d9f82
juanguz1999 <juanguz1999@outlook.com>
2023-06-16 16:48:06 -0500

    Intento subir user 2.0.5

--- src/main/java/com/colegios_peruanos/conectados/controladores/usuarioController.java
+++ src/main/java/com/colegios_peruanos/conectados/controladores/usuarioController.java
@@ -4,6 +4,8 @@ import com.colegios_peruanos.conectados.dao.usuarioDao;
 import com.colegios_peruanos.conectados.modelos.Usuario;
 import com.colegios_peruanos.conectados.servicio.usuarioServicio;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -14,18 +16,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class usuarioController {
     @Autowired
     private usuarioDao usuarioDao;
-    private usuarioServicio usuarioServicio;
 
     @PostMapping("/guardarUsuario")
-    public String guardarUsuario(Usuario usuario) {
+    public String guardarUsuario(HttpServletRequest request) {
+        String correoElectronico = request.getParameter("correoElectronico");
+
+        Usuario usuario = new Usuario();
         usuario.setId(null);
         usuario.setNombre(null);
         usuario.setApellido(null);
-        usuario.setCorreoElectronico(null);
+        usuario.setCorreoElectronico(correoElectronico);
         usuario.setContrasena(null);
         usuario.setTipoUsuario(null);
         usuario.setFechaRegistro(null);
+
         usuarioDao.save(usuario);
+
         return "/vistaprincipal";
     }
 
