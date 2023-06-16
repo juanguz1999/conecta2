4d259fc7a5395264d5b21da388e1b0cfe03079cc
juanguz1999 <juanguz1999@outlook.com>
2023-06-16 18:17:07 -0500

    Intento subir user 2.0.9 - Funciona Pero con boton

---
+++ src/main/resources/static/assets/GoogleSignIn/ControlUserGooglePerfil.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+
+//Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDX8Uc4Po4-rPKrzxh2JWkXxd16O7I1Eik",
+    authDomain: "conectadosp-6c872.firebaseapp.com",
+    projectId: "conectadosp-6c872",
+    storageBucket: "conectadosp-6c872.appspot.com",
+    messagingSenderId: "803161019115",
+    appId: "1:803161019115:web:cff53e5d1c2c5a285e24ae",
+    measurementId: "G-7VG5NV4KJK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // El usuario ha iniciado sesión
+        console.log(user);
+        console.log(user.photoURL);
+        document.querySelector("#user-name").textContent = user.displayName;
+        document.querySelector("#profile-picture").src = user.photoURL;
+        document.querySelector("#nombreUsuario").textContent = user.displayName;
+        document.querySelector("#correoUsuario").textContent = user.email;
+        document.querySelector("#numeroTelefono").textContent = user.phoneNumber;
+        document.querySelector("#fotoPerfil").src = user.photoURL;
+
+
+    } else {
+        // El usuario no ha iniciado sesión
+        document.querySelector("#user-name").textContent = "";
+    }
+});
+
+document.querySelector("#sign-out-button").addEventListener("click", () => {
+    auth.signOut().then(() => {
+        location.replace('/');
+    });
+});
