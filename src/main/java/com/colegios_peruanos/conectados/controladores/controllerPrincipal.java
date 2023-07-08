bec772e2686ae20e85e1edadc98942a8b125b4f9
juanguz1999 <juanguz1999@outlook.com>
2023-07-07 23:25:56 -0500

    Asignacion Aula

--- src/main/java/com/colegios_peruanos/conectados/controladores/controllerPrincipal.java
+++ src/main/java/com/colegios_peruanos/conectados/controladores/controllerPrincipal.java
@@ -1,10 +1,26 @@
 package com.colegios_peruanos.conectados.controladores;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.colegios_peruanos.conectados.modelos.Curso;
+import com.colegios_peruanos.conectados.modelos.Grado;
+import com.colegios_peruanos.conectados.servicio.gradoServicio;
+import com.colegios_peruanos.conectados.servicio.cursoServicio;
 
 @Controller
 public class controllerPrincipal {
+
+	@Autowired
+	private gradoServicio gradoservicio;
+
+	@Autowired
+	private cursoServicio cursoServicio;
+
 	@GetMapping("/")
 	String Index() {
 		return "index";
@@ -51,7 +67,10 @@ public class controllerPrincipal {
 	}
 
 	@GetMapping("/asignarAula")
-	String asignarAula() {
+	String asignarAula(Model model) {
+
+		List<Curso> cursos = cursoServicio.listar();
+		model.addAttribute("cursos", cursos);
 		return "/asignarAula";
 	}
 
